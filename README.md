7d484d2c771a2ddde83060fa0e73f4a6f5323c22
Nitin Sharma <nsharma78@gmail.com>
2017-11-12 18:22:41 -0800

    11/12/2017

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Graphs
+
+1) Clone Graph
+2) Course Schedule , Course Schedule II , Minimum Height Trees
+3) Reconstruct Itinerary
+4) Graph Valid Tree
---
+++ src/main/java/com/nitin/algo/CloneGraph.java
@@ -0,0 +1,15 @@
+/*
+ * Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+ * a queue is used to do breath first traversal,
+ * and a map is used to store the visited nodes. It is the map between original node and copied node.
+ */
+
+package com.nitin.algo;
+
+public class CloneGraph {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
