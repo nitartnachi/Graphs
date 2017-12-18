f39066c954c80070844fc62d018e12baf94d36d6
Nitin Sharma <nsharma78@gmail.com>
2017-12-17 19:04:13 -0800

    ReconstructItineraryV2 implemented

--- src/main/java/com/nitin/algo/ReconstructItineraryV2.java
+++ src/main/java/com/nitin/algo/ReconstructItineraryV2.java
@@ -26,10 +26,10 @@ public class ReconstructItineraryV2 {
 	
 	public static void main(String[] args) {
 		 Map<String, String> tickets = new HashMap<String, String>();
-		 tickets.put(" Chennai", "Banglore");
+		 tickets.put("Chennai", "Banglore");
 		 tickets.put("Bombay", "Delhi");
 		 tickets.put("Goa", "Chennai");
-		 tickets.put("Delhi", "Delhi");
+		 tickets.put("Delhi", "Goa");
 		 System.out.println("Itinerary: " + reconstructItinerary(tickets).toString());
 	}
 
@@ -44,7 +44,25 @@ public class ReconstructItineraryV2 {
 		for(Map.Entry<String, String> ticket : tickets.entrySet())
 			reverse.put(ticket.getValue(), ticket.getKey());
 		
+		String from = null;
+		//find starting point
+		for(String ticket : tickets.keySet())
+			if(!reverse.containsKey(ticket)) {
+				from = ticket;
+				result.add(from);
+				break;
+			}
 		
+		if(from == null)
+			throw new IllegalArgumentException("Invalid start");
+		
+		String to = tickets.get(from);
+		
+		while(to != null){
+			result.add(to);
+			from = to;
+			to = tickets.get(from);
+		}
 		return result;
 	}
 
