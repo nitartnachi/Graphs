5e01eeba64d71ff7a6ec0a25756a70c95ab532c5
Nitin Sharma <nsharma78@gmail.com>
2018-01-15 12:03:50 -0800

    BFS implemented

---
+++ src/main/java/com/nitin/algo/BFS.java
@@ -0,0 +1,46 @@
+/*
+ * BFS traversal
+ */
+package com.nitin.algo;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+import com.nitin.algo.GraphUtils.GraphNode;
+
+public class BFS {
+	
+	public static void main(String[] args) {
+		GraphNode root = GraphNode.createGraph();
+		System.out.println("BFS Traversal");
+		bfsTraversal(root);
+	}
+
+	private static void bfsTraversal(GraphNode root) {
+		if(root == null)
+			return;
+		
+		Set<GraphNode> set = new HashSet<>();
+		Queue<GraphNode> queue = new LinkedList<>();
+		queue.add(root);
+		set.add(root);
+
+		while(!queue.isEmpty()) {
+			
+			GraphNode node = queue.remove();
+			System.out.println(node.val);;
+			node.visited = true;
+			
+			if(node.neighbors != null && !node.neighbors.isEmpty()) {
+				for(GraphNode neighbor : node.neighbors)
+					if(!neighbor.visited && set.add(neighbor)) {
+						queue.add(neighbor);
+					}
+			}
+		}
+		
+	}
+
+}
--- src/main/java/com/nitin/algo/GraphUtils.java
+++ src/main/java/com/nitin/algo/GraphUtils.java
@@ -30,29 +30,31 @@ public class GraphUtils {
 		public String toString(){
 			return "value: "+ this.val; 
 		}
+		
+		public static GraphNode createGraph() {
+			GraphNode n1 = new GraphNode(1); 
+			GraphNode n2 = new GraphNode(2); 
+			GraphNode n3 = new GraphNode(3); 
+			GraphNode n4 = new GraphNode(4); 
+			GraphNode n5 = new GraphNode(5); 
+	 
+			n1.neighbors = new ArrayList<GraphNode>(Arrays.asList(n2,n3,n5));
+			n2.neighbors = new ArrayList<GraphNode>(Arrays.asList(n1,n4));
+			n3.neighbors = new ArrayList<GraphNode>(Arrays.asList(n1,n4,n5));
+			n4.neighbors = new ArrayList<GraphNode>(Arrays.asList(n2,n3,n5));
+			n5.neighbors = new ArrayList<GraphNode>(Arrays.asList(n1,n3,n4));
+			
+			return n1;
+		}
 	}
 	
 	public static void main(String[] args) {
-		GraphNode root = createGraph();
+		GraphNode root = GraphNode.createGraph();
 		System.out.println("BFS Traversal");
 		bfsTraversal(root);
 	}
 	
-	public static GraphNode createGraph() {
-		GraphNode n1 = new GraphNode(1); 
-		GraphNode n2 = new GraphNode(2); 
-		GraphNode n3 = new GraphNode(3); 
-		GraphNode n4 = new GraphNode(4); 
-		GraphNode n5 = new GraphNode(5); 
- 
-		n1.neighbors = new ArrayList<GraphNode>(Arrays.asList(n2,n3,n5));
-		n2.neighbors = new ArrayList<GraphNode>(Arrays.asList(n1,n4));
-		n3.neighbors = new ArrayList<GraphNode>(Arrays.asList(n1,n4,n5));
-		n4.neighbors = new ArrayList<GraphNode>(Arrays.asList(n2,n3,n5));
-		n5.neighbors = new ArrayList<GraphNode>(Arrays.asList(n1,n3,n4));
-		
-		return n1;
-	}
+	
 
 	public static void bfsTraversal(GraphNode root) {
 		if(root == null)
